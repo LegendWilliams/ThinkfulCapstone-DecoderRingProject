4a8fcf26dc129ce9601a06053a6b30a749329f17
LegendWilliams <schencksaivon@gmail.com>
2021-04-22 13:56:06 -0400

    removed extra function declartion that prohibiting the proper functunality of the code

--- src/caesar.js
+++ src/caesar.js
@@ -2,9 +2,6 @@
 // as the index.html and test files rely on this setup to work properly.
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
-
-const caesarModule = (function () {
-  // you can add any code you want within this function scope
 const caesarModule = (() => {
   const BOUNDARIES = {
     start: "a".charCodeAt(),
@@ -15,22 +12,18 @@ const caesarModule = (() => {
     if (shift < -25 || shift > 25) return false;
     if (!encode) shift *= -1;
     input = input.toLowerCase();
-    return input.split("").reduce((result, char) => {
+    return input.split("").reduce((result, char) => { 
       const code = char.charCodeAt();
       if (code < BOUNDARIES.start || code > BOUNDARIES.end) return result + char;
       let shifted = code + shift;
-      
       if (shifted > BOUNDARIES.end) {
         shifted = BOUNDARIES.start + (shifted - BOUNDARIES.end - 1);
       } else if (shifted < BOUNDARIES.start) {
         shifted = BOUNDARIES.end - (BOUNDARIES.start - shifted - 1);
       }
       return result + String.fromCharCode(shifted);
-    }, "");
+    }, ""); 
   }
   return { caesar };
-
 })();
-
 module.exports = { caesar: caesarModule.caesar };
-}
